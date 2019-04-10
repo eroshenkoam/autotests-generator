f46437d720f5e118c0c1e86ce8d6288d65589fff
Artem Eroshenko <eroshenkoam@me.com>
2019-04-10 16:18:40 +0300

    initial commit

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,37 @@
+import io.qameta.allure.Attachment;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.stream.IntStream;
+
+/**
+ * @author eroshenkoam (Artem Eroshenko).
+ */
+public class TestExample {
+
+    static IntStream range() {
+        return IntStream.range(0, 700);
+    }
+
+    @ParameterizedTest
+    @MethodSource("range")
+    public void testOutput() {
+        try (InputStream stream = ClassLoader.getSystemResourceAsStream("attachment.html")) {
+            final String content = IOUtils.toString(stream, Charset.forName("UTF-8"));
+            for (int i = 0; i < 250; i++) {
+                attachContent(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Attachment(value = "imagediff", fileExtension = "json", type = "text/json")
+    public byte[] attachContent(String content) {
+        return content.getBytes();
+    }
+}
